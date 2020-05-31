e55b61cf830f6de811489a82a820a63a53dcb9f4
juan <jpbmdev@gmail.com>
2020-05-31 11:47:13 -0500

    :construction: 2D consumption template created

--- client/restApi/server.js
+++ client/restApi/server.js
@@ -1,17 +1,21 @@
-const {
-  getUserByIdClient
-  } = require("../services/profileClient");
-  
-  //Server rest
-  var rest = require(`express`)();
-  
-  rest.get('/getUserById/:parameter', function (req, res) { 
-    param = req.params.parameter // Asi se saca el paramentro 
-    getUserByIdClient(res, param);
-  });
-  
-  rest.listen(3001, function () {
-    console.log('La api REST que consume el cliente SOAP esta escuchando 3001!');
-  });
-  
-  module.exports = {rest};
+const { getUserByIdClient } = require("../services/profileClient");
+const { get2dSOAPData } = require("../services/otherClient");
+
+//Server rest
+var rest = require(`express`)();
+
+rest.get('/getUserById/:parameter', function (req, res) {
+  param = req.params.parameter // Asi se saca el paramentro 
+  getUserByIdClient(res, param);
+});
+
+rest.get('/2dSOAPConsumption/:parameter', (req, res) => {
+  param = req.params.parameter // Asi se saca el paramentro 
+  get2dSOAPData(res, param);
+});
+
+rest.listen(3001, function () {
+  console.log('La api REST que consume el cliente SOAP esta escuchando 3001!');
+});
+
+module.exports = { rest };
--- client/servers/msOthers.js
+++ client/servers/msOthers.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT_SERVER_SOAP || "8001";
+const PORT = process.env.PORT_SERVER_SOAP || "3010";
 const END_POINT = process.env.END_POINT_SERVER_SOAP || "http://localhost";
 
 const URL = END_POINT + ":" + PORT + "/";
